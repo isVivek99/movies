e160a4d2429a6d68f89b46861e56271ce4f34003
Vivek Lokhande <vivek2811999@gmail.com>
2021-05-15 16:33:37 +0530

    added watchlist feature getting a bug in which local stoeage updates automatically

--- src/movies/Header.js
+++ src/movies/Header.js
@@ -6,18 +6,13 @@ function Header() {
     return (
         <header>
             <div className="header-container">
-                <div className="header-inner-content">
-                    <div className="header-brand">
-                        <Link to="/">Movies</Link>
-                    </div>
 
+                    
                     <ul className="header-nav-links">
                         <li>
                             <Link to="/">Movies</Link>
                         </li>
-                        <li>
-                            <Link to="/watched">Watched</Link>
-                        </li>
+                
                         <li>
                             <Link to="/watchlist">Watchlist</Link>
                         </li>
